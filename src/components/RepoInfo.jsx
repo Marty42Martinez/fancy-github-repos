dfa06324febbda863dfebaa58a483ca8d025d2fb
Marty Martinez <marty.martinez@equipmentshare.com>
2022-10-19 15:09:25 -0700

    user stories from interview

--- src/components/RepoInfo.jsx
+++ src/components/RepoInfo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './RepoInfo.css';
 
-export default function RepoInfo({repoName, starCount}) {
+export default function RepoInfo({repoName, starCount, forkCount, url, handleClick}) {
   return (
-    <div className='Repository'>
+    <div className='Repository' onClick={() => handleClick(repoName, starCount, forkCount, url)}>
       <div>Name: {repoName}</div>
       <div>Star count: {starCount}</div>
     </div>
